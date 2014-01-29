d3d25eb6cbb3aa78f96b13496beb5fc98681dd6c
dianaliu <diana.g.liu@gmail.com>
2014-01-28 23:40:38 -0500

    Add key for upcoming and past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :twitter_handle, uniqueness: true
+
+  def is_past?
+    start_date.present? && start_date < Date.today
+  end
+
+  def is_upcoming?
+    start_date.present? && start_date <= Date.today + 3.months
+  end
 end
