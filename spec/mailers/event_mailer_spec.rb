a2caac4cb14ab700a7bf81351faf42a6787804c7
dianaliu <diana.g.liu@gmail.com>
2014-02-03 00:54:54 -0500

    Add EventMailer specs

--- spec/mailers/event_mailer_spec.rb
+++ spec/mailers/event_mailer_spec.rb
@@ -1,5 +1,30 @@
 require "spec_helper"
 
 describe EventMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".weekly_digest" do
+    it "sends an email" do
+      changes = { :updated => [], :created => [] }
+      EventMailer.weekly_digest(changes).deliver
+
+      expect(ActionMailer::Base.deliveries).to_not be_empty
+    end
+
+    it "sends the correct content" do
+      pot = Event.create(:twitter_handle => 'pot', :name => "black pot")
+      kettle = Event.create(:twitter_handle => 'kettle', :name => "kettle")
+      kettle.update_attribute(:name, 'black kettle')
+      changes = { :updated => [kettle], :created => [pot] }
+
+      email = EventMailer.weekly_digest(changes).deliver
+
+      expect(email.from).to include('no-reply@ls-tech-events.herokuapp.com')
+      expect(email.to).to include(ENV['ADMIN_EMAIL'])
+      expect(email.subject).to eq('New and updated tech events')
+
+      # TODO: Move fixture under spec/
+      fixture = read_fixture('weekly_digest').join
+      expect(fixture).to include(email.text_part.decoded)
+      expect(fixture).to include(email.html_part.decoded)
+    end
+  end
 end
---
+++ spec/tasks/scheduler_rake_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'rake'
+
+describe 'scheduler rake task' do
+  before :all do
+    Rake.application.rake_require "tasks/scheduler"
+    Rake::Task.define_task(:environment)
+  end
+
+  let :run_rake_task do
+    Rake::Task['update_from_twitter'].reenable
+    Rake.application.invoke_task 'update_from_twitter'
+  end
+
+  it 'should email when there are changes on Friday' do
+    Time.stub_chain(:now, :friday?).and_return(:true)
+
+    # FIXME: Both sides fail with cryptic messages
+    # expect{ run_rake_task }.to change{ ActionMailer::Base.deliveries.size }.by(1)
+
+    pending "Fix rake environment bugs"
+  end
+
+  it 'should not email when there are no changes' do
+    pending "Fix rake environment bugs"
+  end
+end
