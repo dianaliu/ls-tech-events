5a9526fb59e6944659a07826b437dc437f16bcde
dianaliu <diana.g.liu@gmail.com>
2014-02-17 14:18:55 -0500

    Add spec for rake task update_from_twitter

--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -9,17 +9,17 @@ task :update_from_twitter => :environment do |t|
       :access_token_secret => ENV['TWITTER_ACCESS_TOKEN_SECRET']
       })
 
-    members = JSON.parse(client.list_members(103196694).to_json)
+    members = JSON.parse(client.list_members(103196694).to_json, :symbolize_names => true)
     members.each do |m|
-      e = Event.find_or_initialize_by(twitter_handle: m['screen_name'])
+      e = Event.find_or_initialize_by(twitter_handle: m[:screen_name])
       changes[:created] << e if e.new_record?
       e.update({
-        :name => m['name'],
-        :description => m['description'],
-        :location => m['location'],
-        :twitter_handle => m['screen_name'],
-        :website_url => m['entities']['url']['urls'][0]['expanded_url'],
-        :logo => m['profile_image_url']
+        :name => m[:name],
+        :description => m[:description],
+        :location => m[:location],
+        :twitter_handle => m[:screen_name],
+        :website_url => m[:entities][:url][:urls][0][:expanded_url],
+        :logo => m[:profile_image_url]
         })
 
       changes[:updated] << e if e.previous_changes.any? && !changes[:created].include?(e)
@@ -30,7 +30,7 @@ task :update_from_twitter => :environment do |t|
 end
 
 desc "email users who have upcoming events"
-task :remind_users => environment do |t|
+task :remind_users => :environment do |t|
   if Date.today == Date.today.beginning_of_month
     # Users who have never been reminded or were reminded more than 3 months ago
     users = User.where("last_reminder_date >= :start_date", { start_date: 3.months.ago }) && User.where(:last_reminder_date => nil)
--- spec/tasks/scheduler_rake_spec.rb
+++ spec/tasks/scheduler_rake_spec.rb
@@ -2,32 +2,44 @@ require 'spec_helper'
 require 'rake'
 
 describe 'scheduler rake task' do
-  # before :all do
-  #   Rake.application.rake_require "tasks/scheduler"
-  #   Rake::Task.define_task(:environment)
-  # end
+  before :all do
+    Rake::Task.define_task(:environment)
+    LsTechEvents::Application.load_tasks
+  end
 
-  # describe "update_from_twitter" do
-  #   let :run_rake_task do
-  #     Rake::Task['update_from_twitter'].reenable
-  #     Rake.application.invoke_task 'update_from_twitter'
-  #   end
+  describe "update_from_twitter" do
+    let :run_rake_task do
+      Rake::Task['update_from_twitter'].reenable
+      Rake.application.invoke_task 'update_from_twitter'
+    end
 
-  #   it 'should email when there are changes on Friday' do
-  #     Time.stub_chain(:now, :friday?).and_return(:true)
+    it 'should email when there are changes on a Friday' do
+      Twitter::REST::Client.any_instance.stub(:list_members).and_return(JSON.parse(File.read('test/fixtures/tasks/update_from_twitter')))
 
-  #     # FIXME: Both sides fail with cryptic messages
-  #     # expect{ run_rake_task }.to change{ ActionMailer::Base.deliveries.size }.by(1)
+      Time.any_instance.stub(:friday?).and_return(true)
+      expect(Time.now.friday?).to eq(true)
 
-  #     pending "Fix rake environment bugs"
-  #   end
+      expect(EventMailer).to receive(:weekly_digest).and_return( double("Mailer", :deliver => true) )
+      expect{ run_rake_task }.to change{ Event.count }.by(1)
+    end
 
-  #   it 'should not email when there are no changes' do
-  #     pending "Fix rake environment bugs"
-  #   end
-  # end
+    it 'should not email when there are no changes' do
+      Twitter::REST::Client.any_instance.stub(:list_members).and_return([])
 
-  # describe "remind_users" do
-  #   pending
-  # end
+      Time.any_instance.stub(:friday?).and_return(true)
+      expect(Time.now.friday?).to eq(true)
+
+      expect(EventMailer).to_not receive(:weekly_digest)
+      expect{ run_rake_task }.to_not change{ Event.count }
+    end
+
+    it 'should not email when not a Friday' do
+      expect(Twitter::REST::Client).to_not receive(:new)
+      run_rake_task
+    end
+  end
+
+  describe "remind_users" do
+    pending
+  end
 end
