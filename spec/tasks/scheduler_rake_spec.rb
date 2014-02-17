b310111fa82a57006e756c6f30a60102405c4949
dianaliu <diana.g.liu@gmail.com>
2014-02-17 14:57:41 -0500

    Add spec for rake remind_users

--- spec/tasks/scheduler_rake_spec.rb
+++ spec/tasks/scheduler_rake_spec.rb
@@ -19,7 +19,7 @@ describe 'scheduler rake task' do
       Time.any_instance.stub(:friday?).and_return(true)
       expect(Time.now.friday?).to eq(true)
 
-      expect(EventMailer).to receive(:weekly_digest).and_return( double("Mailer", :deliver => true) )
+      expect(EventMailer).to receive(:weekly_digest).and_call_original
       expect{ run_rake_task }.to change{ Event.count }.by(1)
     end
 
@@ -40,6 +40,30 @@ describe 'scheduler rake task' do
   end
 
   describe "remind_users" do
-    pending
+    let :run_rake_task do
+      Rake::Task['remind_users'].reenable
+      Rake.application.invoke_task 'remind_users'
+    end
+
+    it "should run on the first day of the month" do
+      Date.any_instance.stub(:beginning_of_month).and_return(Date.today)
+      expect(User).to receive(:where).exactly(2).times.and_call_original
+      run_rake_task
+    end
+
+    it "should not run at any other time" do
+      Date.any_instance.stub(:beginning_of_month).and_return(Date.new(1356, 2, 15))
+      expect(User).to_not receive(:where)
+      run_rake_task
+    end
+
+    it "should email users who need a reminder" do
+      Date.any_instance.stub(:beginning_of_month).and_return(Date.today)
+
+      User.stub(:where).and_return([User.create(:email => 'lestrade@gmail.com', :password => 'sherlocked')])
+      expect(UserMailer).to receive(:events_reminder).and_call_original
+
+      run_rake_task
+    end
   end
 end
